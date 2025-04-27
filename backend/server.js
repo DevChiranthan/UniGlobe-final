2c8a6f7ae2fd64b83deb3db393c6610a2a4fb8dc
DevChiranthan <reddychirantan@gmail.com>
2025-04-27 17:33:07 +0530

    fiix

--- backend/server.js
+++ backend/server.js
@@ -57,5 +57,9 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message || 'Server error' });
 });
 
-const PORT = process.env.PORT || 5000;
+// Choose the appropriate port
+const PORT = process.env.NODE_ENV === 'development' 
+  ? process.env.DEV_PORT || 5001 
+  : process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
